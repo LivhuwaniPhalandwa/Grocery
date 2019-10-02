7ea3399a2319c6d5ec5b83af3bc429db2b6fe57d
sabeloDlangalala <sabeloproffesor3@gmail.com>
2019-10-02 13:58:41 +0200

    pull

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,99 +1,71 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, NavParams, ToastController } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
-import { LoadingController } from 'ionic-angular';
-import * as firebase from 'firebase';
-import { CameraOptions } from '@ionic-native/camera';
-
-
-
+ import { NavController } from 'ionic-angular';
+import firebase from 'firebase';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  toggle: boolean;
- Storage =firebase.storage;
- database=firebase.firestore();
- Items=[];
-
- item = {
-  name:"",
-  price:null,
-  quantity:0,
-  
- }
-  
- 
- Picture: string;
-  Picture_url: string;
- 
-  
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController,public navParams: NavParams, public alertCtrl: AlertController, private camera: Camera, public loadingCtrl: LoadingController) {
-    
-  }
-  addData(){
-    this.database.collection("Item").doc().set(this.item).then(res => {
-      this.toastCtrl.create({
-        message: 'Item added',
-        duration: 2000
-      }).present()
-    }).catch(err => {
-      this.toastCtrl.create({
-        message: 'Error adding item',
-        duration: 2000
-      }).present()
-    })
-  }
-  incrementQ(){
-    this.item.quantity = this.item.quantity + 1
-  }
-  decrementQ(){
-    this.item.quantity = this.item.quantity - 1
-  }
-
-  takePicture(sourcetype: number) {
-    console.log(';;;;;;;;;');
-  
-    const options: CameraOptions = {
-      quality: 50,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      sourceType: sourcetype,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true,
-      targetHeight: 500,
-      targetWidth: 500
-    }
-    this.camera.getPicture(options).then((picture) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64 (DATA_URL):
-     this.Picture = 'data:image/jpeg;base64,' + picture;
-     console.log('IMG: ',this.Picture);
-    }, (err) => {
-      console.log('error: ', err);
-     // Handle error
-    });
-  
-  let storageRef = firebase.storage().ref();
-  const filename = Math.floor(Date.now() / 1000);
-  let file = 'my-hotel/'+filename+'.jpg';
-  const imageRef = storageRef.child(file);
-  imageRef.putString(this.Picture, firebase.storage.StringFormat.DATA_URL)
-  .then((snapshot) => {
-    console.log('image uploaded');
-    this.Picture_url = snapshot.downloadURL;
-    let alert = this.alertCtrl.create({
-      title: 'Image Upload', 
-      subTitle: 'Image Uploaded to firebase',
-      buttons: ['Ok']
-    }).present();
-  })
-
-}  
- 
- 
+ toggle: boolean;
+ Items = [];
+item={
+  name:'',
+  price: '',
+  quantity: ''
 }
 
+  database=firebase.firestore();
+ 
+ 
+  
+  constructor(public navCtrl: NavController) {
+
+  }
+  ionViewDidLoad() {
+    this.pullData();
+  }
+  // addData(){
+  //    this.database.collection("Item").get().then((res)=>{
+  //     //console.log(res.);
+  //      res.forEach((doc)=>{
+  //         console.log(doc.data());
+         
+  //      })
+  //    })
+  // }
+
+  
+  pullData(){
+    let data = {
+      docid: "",
+      doc: {}
+    }
+     this.database.collection("Item").onSnapshot(doc => {
+           doc.forEach(item => {
+             data.docid = item.id
+             data.doc = item.data();
+             data = {
+              docid: "",
+              doc: {}
+            }
+             
+             this.Items.push(data);
+           })
+           console.log("Your data is", this.Items)
+    })
+  }
+
+  deleteData(docid){
+     this.database.collection("Item").doc(docid).delete();
+    this.Items = []
+     this.pullData()
+  }
+
+  
+  expandDiv(){
+    this.toggle = !this.toggle;
+  }
 
 
+  
+}
